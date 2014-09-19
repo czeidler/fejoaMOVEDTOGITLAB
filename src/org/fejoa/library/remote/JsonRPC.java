bb7fc917e6c73b54b07909c39705b6f82c38651a
czeidler <mail@fejoa.org>
2014-09-19 20:43:27 +1200

    Add empty PublishMessageBranch request and add JsonRPC.

---
+++ src/org/fejoa/library/remote/JsonRPC.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package org.fejoa.library.remote;
+
+import com.google.gson.Gson;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+
+public class JsonRPC {
+    static private int globalJsonId = 0;
+    private int jsonId = 0;
+
+    static public class Argument {
+        public Argument(String name, Object value) {
+            this.name = name;
+            this.value = new Gson().toJson(value);
+        }
+
+        public Argument(String name, int value) {
+            this.name = name;
+            this.value = Integer.toString(value);
+        }
+
+        public Argument(String name, float value) {
+            this.name = name;
+            this.value = Float.toString(value);
+        }
+
+        public Argument(String name, double value) {
+            this.name = name;
+            this.value = Double.toString(value);
+        }
+
+        public Argument(String name, String value) {
+            this.name = name;
+            this.value = "\"" + value + "\"";
+        }
+
+        String name;
+        String value;
+    }
+
+    public JsonRPC() {
+        setNewJsonId();
+    }
+
+    public String call(String method, Argument ... argumentList) {
+        return call(getJsonId(), method, argumentList);
+    }
+
+    public JSONObject getReturnValue(String jsonString) throws IOException, JSONException {
+        JSONObject jsonObject = new JSONObject(jsonString);
+        if (!jsonObject.getString("jsonrpc").equals("2.0"))
+            throw new IOException("json rpc 2.0 expected");
+        if (jsonObject.getInt("id") != jsonId)
+            throw new IOException("wrong method call id; got: " + jsonObject.getInt("id") + " expected: " + jsonId);
+        return jsonObject.getJSONObject("result");
+    }
+
+    protected int setNewJsonId() {
+        globalJsonId++;
+        jsonId = globalJsonId;
+        return jsonId;
+    }
+
+    protected int getJsonId() {
+        return jsonId;
+    }
+
+    protected String call(int jsonId, String method, Argument ... argumentList) {
+        String request = "{ \"jsonrpc\": \"2.0\", \"method\":\"" + method + "\"";
+        if (argumentList.length > 0) {
+            request += ",\"params\": {";
+            for (int i = 0; i < argumentList.length; i++) {
+                Argument argument = argumentList[i];
+                request += "\"" + argument.name + "\":" + argument.value + "";
+                if (i < argumentList.length - 1)
+                    request += ",";
+            }
+            request += "}";
+        }
+        request += ", \"id\":" + jsonId + "}";
+        return request;
+    }
+}
+
---
+++ src/org/fejoa/library/remote/PublishMessageBranch.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package org.fejoa.library.remote;
+
+import org.fejoa.library.Contact;
+import org.fejoa.library.UserIdentity;
+import org.fejoa.library.mailbox.MessageBranch;
+import org.fejoa.library.mailbox.MessageBranchInfo;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import rx.Observable;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PublishMessageBranch {
+    private MessageBranch messageBranch;
+
+    public PublishMessageBranch(MessageBranch messageBranch) {
+        this.messageBranch = messageBranch;
+    }
+
+    public Observable<RemoteConnectionJob.Result> publish() {
+        MessageBranchInfo info = messageBranch.getMessageBranchInfo();
+        List<MessageBranchInfo.Participant> participantList = info.getParticipants();
+        UserIdentity userIdentity = messageBranch.getIdentity();
+
+        List<Observable<RemoteConnectionJob.Result>> observableList = new ArrayList<>();
+        for (MessageBranchInfo.Participant participant : participantList) {
+            ConnectionInfo connectionInfo = null;
+            RemoteConnectionJob remoteConnectionJob = null;
+            Contact contactPublic = userIdentity.getContactFinder().find(participant.uid);
+            if (participant.uid.equals("") || contactPublic == null) {
+                //contact request
+                String[] parts = participant.address.split("@");
+                if (parts.length != 2)
+                    continue;
+                String server = parts[1];
+                String serverUser = parts[0];
+
+                connectionInfo = new ConnectionInfo(server, serverUser, userIdentity.getMyself());
+                ContactRequest contactRequest = new ContactRequest(connectionInfo, messageBranch.getIdentity());
+
+                remoteConnectionJob = contactRequest.getContactRequestJob();
+            } else if (connectionInfo != null) {
+                connectionInfo = new ConnectionInfo(contactPublic.getServer(), contactPublic.getServerUser(),
+                        userIdentity.getMyself());
+            }
+
+            PublishMessageBranchJob publishMessageBranchJob = new PublishMessageBranchJob();
+            if (remoteConnectionJob != null)
+                remoteConnectionJob.setFollowUpJob(publishMessageBranchJob);
+            else
+                remoteConnectionJob = publishMessageBranchJob;
+
+            final RemoteConnection remoteConnection = ConnectionManager.get().getConnection(connectionInfo);
+            observableList.add(remoteConnection.queueJob(remoteConnectionJob));
+        }
+
+        return Observable.merge(Observable.from(observableList));
+    }
+
+    class PublishMessageBranchJob extends RemoteConnectionJob {
+        private JsonRPC jsonRPC = new JsonRPC();
+
+        @Override
+        public byte[] getRequest() throws Exception {
+            return jsonRPC.call("publish_branch",
+                    new JsonRPC.Argument("branch", messageBranch.getMessageBranchInfo().getUid())).getBytes();
+        }
+
+        @Override
+        public Result handleResponse(byte[] reply) throws Exception {
+            JSONObject result = jsonRPC.getReturnValue(new String(reply));
+            if (result == null)
+                throw new IOException("bad return value");
+
+            if (!result.has("status"))
+                throw new IOException("no status field in return");
+            String message = "";
+            if (result.has("message"))
+                message = result.getString("message");
+
+            int status = result.getInt("status");
+            if (status != 0)
+                return new Result(Result.ERROR, message);
+
+            return new Result(Result.DONE, message);
+        }
+    }
+}

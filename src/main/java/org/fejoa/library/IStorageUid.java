bb2b034dfbddad1ae21201921d9e91eef3ff2ed0
czeidler <mail@fejoa.org>
2015-04-12 19:54:40 +1200

    Add missing file.

---
+++ src/main/java/org/fejoa/library/IStorageUid.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2014.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package org.fejoa.library;
+
+import org.fejoa.library.database.StorageDir;
+
+
+public interface IStorageUid {
+    String getUid();
+    StorageDir getStorageDir();
+}

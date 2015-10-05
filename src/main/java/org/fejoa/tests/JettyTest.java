43fb1c1f4f89e4b95b0db31cc1f7c5e04ae0d701
Clemens Zeidler <clemens.zeidler@gmail.com>
2015-10-06 11:16:45 +1300

    Jetty test update.

--- src/main/java/org/fejoa/tests/JettyTest.java
+++ src/main/java/org/fejoa/tests/JettyTest.java
@@ -1,13 +1,15 @@
 package org.fejoa.tests;
 
 import junit.framework.TestCase;
-import org.fejoa.library.remote2.ConnectionManager;
-import org.fejoa.library.remote2.GitPushJob;
-import org.fejoa.library.remote2.JsonPingJob;
-import org.fejoa.library.remote2.RemoteJob;
+import org.fejoa.library.crypto.CryptoSettings;
+import org.fejoa.library.database.JGitInterface;
+import org.fejoa.library.remote2.*;
+import org.fejoa.library.support.StorageLib;
 import org.fejoa.server.JettyServer;
 import rx.Observer;
 import rx.concurrency.Schedulers;
+
+import java.io.File;
 
 
 public class JettyTest extends TestCase {
@@ -40,9 +42,37 @@ public class JettyTest extends TestCase {
 
         connectionManager.submit(new JsonPingJob(), connectionInfo, authInfo, observer);
 
-        connectionManager.submit(new GitPushJob(), connectionInfo, authInfo, observer);
+        // push
+        /*JGitInterface gitInterface = new JGitInterface();
+        gitInterface.init(".gitTestClient", "testBranch", true);
+        gitInterface.writeBytes("testFile", "testData".getBytes());
+        gitInterface.commit();
+        connectionManager.submit(new GitPushJob(gitInterface.getRepository(), gitInterface.getBranch()),
+                connectionInfo, authInfo, observer);
+*/
+
+        // pull
+        /*
+        JGitInterface gitInterface = new JGitInterface();
+        gitInterface.init(".gitTest", "testBranch", true);
+        gitInterface.writeBytes("testFileServer", "testDataServer".getBytes());
+        gitInterface.commit();
+
+        JGitInterface gitClientInterface = new JGitInterface();
+        gitClientInterface.init(".gitTestClient", "testBranch", true);
+        connectionManager.submit(new GitPullJob(gitClientInterface.getRepository(), gitClientInterface.getBranch()),
+                connectionInfo, authInfo, observer);
+*/
+        connectionManager.submit(new CreateAccountJob("userName", "password",
+                CryptoSettings.getDefault().masterPassword), connectionInfo, authInfo, observer);
+        Thread.sleep(1000);
+
+        connectionManager.submit(new RootLoginJob("userName", "password"), connectionInfo, authInfo, observer);
 
         Thread.sleep(10000);
         server.stop();
+
+        StorageLib.recursiveDeleteFile(new File(".gitTestClient"));
+        StorageLib.recursiveDeleteFile(new File(".gitTest"));
     }
 }

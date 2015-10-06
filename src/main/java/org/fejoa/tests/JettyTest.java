2f7cbff92e0dab86999f5e4ecc3b9ecd91064a35
Clemens Zeidler <clemens.zeidler@gmail.com>
2015-10-07 12:44:20 +1300

    Update the test case.

--- src/main/java/org/fejoa/tests/JettyTest.java
+++ src/main/java/org/fejoa/tests/JettyTest.java
@@ -10,17 +10,33 @@ import rx.Observer;
 import rx.concurrency.Schedulers;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class JettyTest extends TestCase {
-    public void testSimple() throws Exception {
-        JettyServer server = new JettyServer();
+    final static String TEST_DIR = "jettyTest";
+    final static String SERVER_TEST_DIR = TEST_DIR + "/Server";
 
+    final List<String> cleanUpDirs = new ArrayList<String>();
+    JettyServer server;
+    ConnectionManager.ConnectionInfo connectionInfo;
+    ConnectionManager.AuthInfo authInfo;
+    Observer<RemoteJob.Result> observer;
+    ConnectionManager connectionManager;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        cleanUpDirs.add(TEST_DIR);
+
+        server = new JettyServer(SERVER_TEST_DIR);
         server.start();
 
-        ConnectionManager.ConnectionInfo connectionInfo = new ConnectionManager.ConnectionInfo("", "http://localhost:8080/");
-        ConnectionManager.AuthInfo authInfo = new ConnectionManager.AuthInfo(ConnectionManager.AuthInfo.NONE, null);
-        Observer<RemoteJob.Result> observer = new Observer<RemoteJob.Result>() {
+        connectionInfo = new ConnectionManager.ConnectionInfo("", "http://localhost:8080/");
+        authInfo = new ConnectionManager.AuthInfo(ConnectionManager.AuthInfo.NONE, null);
+        observer = new Observer<RemoteJob.Result>() {
             @Override
             public void onCompleted() {
 
@@ -37,42 +53,62 @@ public class JettyTest extends TestCase {
             }
         };
 
-        ConnectionManager connectionManager = new ConnectionManager(null);
+        connectionManager = new ConnectionManager(null);
         connectionManager.setObserverScheduler(Schedulers.immediate());
+    }
 
-        connectionManager.submit(new JsonPingJob(), connectionInfo, authInfo, observer);
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+
+        Thread.sleep(1000);
+        server.stop();
+
+        for (String dir : cleanUpDirs)
+            StorageLib.recursiveDeleteFile(new File(dir));
+    }
+
+    public void testSync() throws Exception {
+        String serverUser = "user1";
+        String localDir = TEST_DIR + "/.git";
+        String BRANCH = "testBranch";
 
         // push
-        /*JGitInterface gitInterface = new JGitInterface();
-        gitInterface.init(".gitTestClient", "testBranch", true);
+        JGitInterface gitInterface = new JGitInterface();
+        gitInterface.init(localDir, BRANCH, true);
         gitInterface.writeBytes("testFile", "testData".getBytes());
         gitInterface.commit();
-        connectionManager.submit(new GitPushJob(gitInterface.getRepository(), gitInterface.getBranch()),
+        connectionManager.submit(new GitPushJob(gitInterface.getRepository(), serverUser, gitInterface.getBranch()),
                 connectionInfo, authInfo, observer);
-*/
 
-        // pull
-        /*
-        JGitInterface gitInterface = new JGitInterface();
-        gitInterface.init(".gitTest", "testBranch", true);
-        gitInterface.writeBytes("testFileServer", "testDataServer".getBytes());
+        Thread.sleep(1000);
+
+        // do changes on the server
+        JGitInterface serverGit = new JGitInterface();
+        serverGit.init(SERVER_TEST_DIR + "/" + serverUser + "/.git", BRANCH, false);
+        serverGit.writeBytes("testFileServer", "testDataServer".getBytes());
+        serverGit.commit();
+
+        // merge
+        gitInterface.writeBytes("testFile2", "testDataClient2".getBytes());
         gitInterface.commit();
 
-        JGitInterface gitClientInterface = new JGitInterface();
-        gitClientInterface.init(".gitTestClient", "testBranch", true);
-        connectionManager.submit(new GitPullJob(gitClientInterface.getRepository(), gitClientInterface.getBranch()),
-                connectionInfo, authInfo, observer);
-*/
+        // pull
+        connectionManager.submit(new GitSyncJob(gitInterface.getRepository(), serverUser,
+                gitInterface.getBranch()), connectionInfo, authInfo, observer);
+
+        Thread.sleep(3000);
+    }
+
+    public void testSimple() throws Exception {
+        connectionManager.submit(new JsonPingJob(), connectionInfo, authInfo, observer);
+
         connectionManager.submit(new CreateAccountJob("userName", "password",
                 CryptoSettings.getDefault().masterPassword), connectionInfo, authInfo, observer);
         Thread.sleep(1000);
 
         connectionManager.submit(new RootLoginJob("userName", "password"), connectionInfo, authInfo, observer);
 
-        Thread.sleep(10000);
-        server.stop();
-
-        StorageLib.recursiveDeleteFile(new File(".gitTestClient"));
-        StorageLib.recursiveDeleteFile(new File(".gitTest"));
+        Thread.sleep(1000);
     }
 }

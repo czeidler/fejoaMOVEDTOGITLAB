9fdc7f0e31d1a0930fa49b303de770c041d6ac57
Clemens Zeidler <clemens.zeidler@gmail.com>
2016-08-03 13:47:42 +1200

    Implement a double linked list.

---
+++ src/main/java/org/fejoa/library/support/DoubleLinkedList.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2016.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package org.fejoa.library.support;
+
+
+import java.util.Iterator;
+
+public class LinkedList<T extends LinkedList.Entry> implements Iterable<T> {
+    public static class Entry {
+        protected Entry previous;
+        protected Entry next;
+    }
+
+    private Entry head;
+    private Entry tail;
+    private int nEntries = 0;
+
+    public T getHead() {
+        return (T)head;
+    }
+
+    public T getTail() {
+        return (T)tail;
+    }
+
+    public int size() {
+        return nEntries;
+    }
+
+    public void addBefore(T entry, T next) {
+        nEntries++;
+
+        entry.next = null;
+        entry.previous = null;
+        if (head == null) {
+            head = entry;
+            tail = head;
+            return;
+        }
+        if (next.previous != null)
+            next.previous.next = entry;
+        entry.previous = next.previous;
+        entry.next = next;
+        next.previous = entry;
+        if (next == head)
+            head = entry;
+    }
+
+    public void addFirst(T entry) {
+        addBefore(entry, getHead());
+    }
+
+    public void addLast(T entry) {
+        nEntries++;
+
+        entry.next = null;
+        entry.previous = null;
+        if (head == null) {
+            head = entry;
+            tail = head;
+            return;
+        }
+
+        entry.previous = tail;
+        tail.next = entry;
+        tail = entry;
+    }
+
+    public void remove(Entry entry) {
+        nEntries--;
+
+        if (entry.previous != null) {
+            entry.previous.next = entry.next;
+        } else {
+            head = entry.next;
+        }
+        if (entry == tail)
+            tail = entry.previous;
+
+        if (entry.next != null)
+            entry.next.previous = entry.previous;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Entry current = head;
+
+            @Override
+            public void remove() {
+                LinkedList.this.remove(current);
+                current = current.next;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                Entry entry = current;
+                current = entry.next;
+                return (T)entry;
+            }
+        };
+    }
+
+}
---
+++ src/main/java/org/fejoa/tests/DoubleLinkedListTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
+package org.fejoa.tests;
+
+import junit.framework.TestCase;
+import org.fejoa.library.support.LinkedList;
+
+
+public class LinkedListTest extends TestCase {
+    class TestEntry extends LinkedList.Entry {
+        int i;
+
+        TestEntry(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public String toString() {
+            return "" + i;
+        }
+    }
+
+    private void assertContent(LinkedList<TestEntry> list, String expected) {
+        assertEquals(expected.length(), list.size());
+
+        String result = "";
+        for (TestEntry entry : list)
+            result += entry.toString();
+        assertEquals(expected, result);
+
+        if (list.getTail() != null)
+            assertEquals(list.getTail().toString(), expected.substring(expected.length() - 1));
+    }
+
+    public void testLinkedList() {
+        LinkedList<TestEntry> linkedList = new LinkedList<>();
+
+        TestEntry entry1 = new TestEntry(1);
+        TestEntry entry2 = new TestEntry(2);
+        TestEntry entry3 = new TestEntry(3);
+        TestEntry entry4 = new TestEntry(4);
+        TestEntry entry5 = new TestEntry(5);
+        linkedList.addFirst(entry4);
+        assertContent(linkedList, "4");
+        linkedList.addBefore(entry2, entry4);
+        assertContent(linkedList, "24");
+        linkedList.addBefore(entry3, entry4);
+        assertContent(linkedList, "234");
+        linkedList.addFirst(entry1);
+        assertContent(linkedList, "1234");
+        linkedList.addLast(entry5);
+        assertContent(linkedList, "12345");
+
+        linkedList.remove(entry5);
+        assertContent(linkedList, "1234");
+        linkedList.remove(entry1);
+        assertContent(linkedList, "234");
+        linkedList.remove(entry3);
+        assertContent(linkedList, "24");
+        linkedList.remove(entry4);
+        assertContent(linkedList, "2");
+        linkedList.remove(entry2);
+        assertContent(linkedList, "");
+    }
+}
